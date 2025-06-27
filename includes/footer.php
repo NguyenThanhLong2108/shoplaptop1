072970d265d0312b5b0afb4ccb4dde292002f6c8
NguyenThanhLong2108 <nhthanhlong2108@gmail.com>
2025-06-27 15:45:22 +0700

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,39 @@
+<footer style="margin-top: 30px;">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3">
+          <ul>
+            <li><a href="#">Giới thiệu công ty</a></li>
+            <li><a href="#">Hệ thống cửa hàng</a></li>
+            <li><a href="#">Tuyển dụng</a></li>
+            <li><a href="#">Chính sách bảo mật</a></li>
+          </ul>
+        </div>
+        <div class="col-md-3">
+          <ul>
+            <li><a href="#">Chính sách bảo hành</a></li>
+            <li><a href="#">Chính sách đổi trả sản phẩm</a></li>
+            <li><a href="#">Hỏi Đáp Mua Hàng Online</a></li>
+            <li><a href="#">Phương Thức Thanh Toán</a></li>
+          </ul>
+        </div>
+        <div class="col-md-3">
+          <ul>
+            <li><a href="#">Gọi mua hàng miễn phí: 09999999</a></li>
+            <li><a href="#">Gọi bảo hành & khiếu nại: 09999999</a></li>
+            <li><a href="#">Gọi hợp tác kinh doanh: 09999999</a></li>
+          </ul>
+        </div>
+        <div class="col-md-3">
+          <ul>
+            <li><i class="fab fa-cc-visa" style="font-size: 3rem;margin-right: 5px;"></i><i class="fa fa-credit-card" style="font-size: 3rem;margin-right: 5px;"></i><i class="fab fa-cc-mastercard" style="font-size: 3rem;margin-right: 5px;"></i><i class="fab fa-cc-jcb"  style="font-size: 3rem;margin-right: 5px;"></i></li>
+            <li style="margin-top: 3px;"><span style="border: 1px solid black; border-radius:2px; font-size:1.4rem; color: white; background-color: #29303b; padding: 2px">COD</span></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <hr />
+    <div class="bottom">
+      <p>Toà nhà Đại học Công nghệ Đông Á, P. Trịnh Văn Bô, Xuân Phương, Nam Từ Liêm, Hà Nội 129630, Việt Nam</p>
+    </div>
+  </footer>
